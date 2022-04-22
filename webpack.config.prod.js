401db5bf3f418450e3d634af8c17e0ecde5e1126
Jaison Schmidt <31486070+jaisonschmidt@users.noreply.github.com>
2022-04-22 16:04:32 -0300

    Adjust webpack config files for dev and production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,17 @@
 const { merge } = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const baseConfig = require("./webpack.config.base");
 
 module.exports = merge(baseConfig, {
   mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
 });
