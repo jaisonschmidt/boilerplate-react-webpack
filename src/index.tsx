5ed89dfcc884a224be961ad8cac65136f553534f
Jaison Schmidt <31486070+jaisonschmidt@users.noreply.github.com>
2022-04-24 23:02:09 -0300

    #47: remove some warnings

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
+// TODO: improve this
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const container: any = document.getElementById('app')
 const root = createRoot(container)
 
 if (process.env.NODE_ENV !== 'production') {
+  // TODO: Improve this
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const { default: axe } = require('@axe-core/react')
   axe(React, root, 1000)
