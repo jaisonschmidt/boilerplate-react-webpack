c41d52e2f5405a8ea453ec4a38371516e69587ff
Jaison Schmidt <31486070+jaisonschmidt@users.noreply.github.com>
2022-04-24 22:33:55 -0300

    #46: install and configure react-axe dependency

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 
 const container = document.getElementById('app')
-const root = createRoot(container) // createRoot(container!) if you use TypeScript
+const root = createRoot(container)
+
+if (process.env.NODE_ENV !== 'production') {
+  const { default: axe } = require('@axe-core/react')
+  axe(React, root, 1000)
+}
+
 root.render(
   <React.StrictMode>
     <App />
