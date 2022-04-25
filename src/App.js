cfb2dcd4f272d4825862ddc8aa094f686bbe6302
Jaison Schmidt <31486070+jaisonschmidt@users.noreply.github.com>
2022-04-24 22:33:55 -0300

    #46: install and configure react-axe dependency

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react'
 class App extends React.Component {
   render() {
-    return <h1>Hello world</h1>
+    return (
+      <>
+        <header role="banner">
+          <p>Put company logo, etc. here.</p>
+        </header>
+        <nav role="navigation">
+          <ul>
+            <li>Put navigation here</li>
+          </ul>
+        </nav>
+        <main role="main">
+          <img
+            alt="Small thing in screen"
+            src="https://smaller-pictures.appspot.com/images/dreamstime_xxl_65780868_small.jpg"
+          />
+          <h1>Put main content here.</h1>
+        </main>
+        <footer role="contentinfo">
+          <p>Put copyright, etc. here.</p>
+        </footer>
+      </>
+    )
   }
 }
 
